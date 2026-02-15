5096ee32a0decf99a00cc9dde511c44f25e85b4b
alisohailshaikh <121343436+alisohailshaikh@users.noreply.github.com>
2026-02-15 13:12:04 +0500

    created streamlit ui
    Created new python envrioment and install packages and listed in requirements.txt plus created the streamlit UI

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 #main page to get find multi model pdf embeddings project
+import streamlit as st
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with Multiple PDFS", page_icon=":soccer:", layout="wide")
+    #main header of the page
+    st.header("Chat with Multiple PDFS :soccer:")
+    st.text_input("Enter your question about your documents here: ")
+    
+    with st.sidebar:
+        st.subheader("Your PDF Documents")
+        st.file_uploader("Upload your PDF files here: ")
+        st.button("Upload")
+
+if __name__ == "__main__":
+    main()
